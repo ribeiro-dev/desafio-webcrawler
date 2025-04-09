37cd68ca31f7f027132ed7276e62fa79d322cf00
ribeiro-dev <romilson.2r@gmail.com>
2025-04-09 11:20:03 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// Getting html
+
+HttpClient client = new HttpClient();
+string baseUrl = "https://proxyservers.pro/proxy/list/order/updated/order_dir/desc";
+var response = await client.GetStringAsync(baseUrl);
+
+Console.WriteLine(response);
