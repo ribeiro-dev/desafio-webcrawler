1a8ac05a183ed487ec420c9c6767358aad82930f
ribeiro-dev <romilson.2r@gmail.com>
2025-04-09 11:42:53 -0300

    add: urls generation

--- Program.cs
+++ Program.cs
@@ -1,7 +1,25 @@
-﻿// Getting html
+﻿using HtmlAgilityPack;
+
+// Getting html
 
 HttpClient client = new HttpClient();
 string baseUrl = "https://proxyservers.pro/proxy/list/order/updated/order_dir/desc";
 var response = await client.GetStringAsync(baseUrl);
 
-Console.WriteLine(response);
+
+// Generating pages url
+
+HtmlDocument htmlDoc = new HtmlDocument();
+htmlDoc.LoadHtml(response);
+
+var lastPageLink = htmlDoc.DocumentNode.SelectNodes("//a[contains(@class, 'page-link')]").Last();
+int pagesCount = int.Parse(lastPageLink.InnerText);
+
+List<string> pagesURL = [];
+for (int i = 1; i <= pagesCount; i++)
+{
+  string fullURL = $"{baseUrl}/page/{i}";
+  pagesURL.Add(fullURL);
+  Console.WriteLine(fullURL);
+}
+
