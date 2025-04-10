dc46c81adff50ceb0bdf48f6807fa3107b739094
ribeiro-dev <romilson.2r@gmail.com>
2025-04-10 14:30:21 -0300

    ref: proxy model

--- Models/Proxy.cs
+++ Models/Proxy.cs
@@ -2,8 +2,8 @@ namespace webcrawler.Models;
 
 public class Proxy
 {
-  public string Ip { get; set; }
-  public string Port { get; set; }
-  public string Country { get; set; }
-  public string Protocol { get; set; }
+  public required string Ip { get; set; }
+  public required string Port { get; set; }
+  public required string Country { get; set; }
+  public required string Protocol { get; set; }
 }
