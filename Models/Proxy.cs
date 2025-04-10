aad295f5b41a0b83f03171d0e5e62ce0096ee12d
ribeiro-dev <romilson.2r@gmail.com>
2025-04-10 01:32:51 -0300

    add: proxy model

---
+++ Models/Proxy.cs
@@ -0,0 +1,9 @@
+namespace webcrawler.Models;
+
+public class Proxy
+{
+  public string Ip { get; set; }
+  public string Port { get; set; }
+  public string Country { get; set; }
+  public string Protocol { get; set; }
+}
